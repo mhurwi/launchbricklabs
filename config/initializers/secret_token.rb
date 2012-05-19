668d15ffac2b6301aa0d00d7e52f74cbe6f179d9
mark <mhurwitzus@gmail.com>
2012-05-19 17:12:04 -0500

    initial commit, after redoing the git repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Launchbricklabs::Application.config.secret_token = '1d61973522fde76ad171bbd7a0b4e0633b0f1362dfc9ccf6756d7f4a2f40aaa76e1a6029bfb0f0b2c8b3486223ae5aeeee6e8c1d7ec261b3d1c5ae5c421c0f23'
